4b6c814bd2882df03fa0412043e8c80dc71c6478
Eric Muller <e.mul@free.fr>
2017-09-02 21:54:03 +0200

    cleanup

--- user/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'id', 'first_name', 'last_name', 'email',
-                  'is_staff', 'is_active', 'date_joined', )
